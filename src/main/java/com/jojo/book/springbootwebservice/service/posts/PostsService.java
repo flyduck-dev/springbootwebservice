3aeb387c80a2a7c6fe5225e287646261588d6aa5
flyduck-dev <flyduck0dev@gmail.com>
2023-06-04 19:48:10 +0900

    스프링 시큐리티 작업 세팅

--- src/main/java/com/jojo/book/springbootwebservice/service/posts/PostsService.java
+++ src/main/java/com/jojo/book/springbootwebservice/service/posts/PostsService.java
@@ -1,6 +1,7 @@
 package com.jojo.book.springbootwebservice.service.posts;
 import com.jojo.book.springbootwebservice.domain.posts.Posts;
 import com.jojo.book.springbootwebservice.domain.posts.PostsRepository;
+import com.jojo.book.springbootwebservice.web.dto.PostsListResponseDto;
 import com.jojo.book.springbootwebservice.web.dto.PostsResponseDto;
 import com.jojo.book.springbootwebservice.web.dto.PostsSaveRequestDto;
 import com.jojo.book.springbootwebservice.web.dto.PostsUpdateRequestDto;
@@ -8,6 +9,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -16,6 +19,7 @@ public class PostsService {
 
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
+
         return postsRepository.save(requestDto.toEntity()).getId();
     }
     @Transactional
@@ -30,5 +34,17 @@ public class PostsService {
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
         return new PostsResponseDto(entity);
+    }
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+    @Transactional
+    public void delete(Long id){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        postsRepository.delete(posts);
     }
 }
--- src/main/java/com/jojo/book/springbootwebservice/web/PostsApiController.java
+++ src/main/java/com/jojo/book/springbootwebservice/web/PostsApiController.java
@@ -4,12 +4,7 @@ import com.jojo.book.springbootwebservice.web.dto.PostsSaveRequestDto;
 import com.jojo.book.springbootwebservice.web.dto.PostsUpdateRequestDto;
 import com.jojo.book.springbootwebservice.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -17,6 +12,7 @@ public class PostsApiController {
     private final PostsService postsService;
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto){
+
         return postsService.save(requestDto);
     }
     @PutMapping("/api/v1/posts/{id}")
@@ -25,6 +21,12 @@ public class PostsApiController {
     }
     @GetMapping("/api/v1/posts/{id}")
     public PostsResponseDto findId(@PathVariable Long id){
+
         return postsService.findById(id);
+    }
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id){
+        postsService.delete(id);
+        return id;
     }
 }
